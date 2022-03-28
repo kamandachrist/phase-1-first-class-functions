1bcb7e054b948783e87282b5c8e106be4fc35461
Christ Kamanda <kamandachristo@gmail.com>
2022-03-28 00:09:32 -0400

    prework first class functions

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback){
+    callback()
+}
+
+function returnsANamedFunction(){
+    return function christ(){
+        
+    }
+}
+   
+function returnsAnAnonymousFunction() {
+    return () => "";
+}
